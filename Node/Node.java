b6b075f07bfb9a8dd50f76826bb8638608dfcfcb
Joana311 <deptop725+3@gmail.com>
2024-03-24 14:11:35 +0100

    Created the Node class, with his atributes

---
+++ Node/Node.java
@@ -0,0 +1,48 @@
+package Node;
+
+import java.util.*;
+import Wallet.*;
+import Transaction.*;
+
+public class Node {
+
+    public static Integer count = 0;
+
+    private Integer id;
+    private Wallet wallet;
+    private HashSet<Transaction> transactions = new HashSet<>();
+
+    public Node(Wallet wallet) {
+        this.wallet = wallet;
+        this.id = count;
+        count++;
+    }
+
+    /*_______________________________*/
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Wallet getWallet() {
+        return this.wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    public HashSet<Transaction> getTransactions() {
+        return this.transactions;
+    }
+
+    public void setTransactions(HashSet<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    /*_______________________________*/
+    @Override
+    public String toString() {
+        return this.wallet.toString()
+                + " | @Node#" + String.format("%03d", this.id);
+    }
+}
