2b51d8d80596852592ad83daa400d28acc46dd03
Joana311 <deptop725+3@gmail.com>
2024-03-24 12:09:15 +0100

    Setted the initial Wallet class

---
+++ Wallet/Wallet.java
@@ -0,0 +1,49 @@
+package Wallet;
+
+public class Wallet {
+    private String name;
+    private String hash;
+    private Integer balance;
+    
+    public Wallet(String name, String hash, Integer balance) {
+        this.name = name;
+        this.hash = hash;
+        this.balance = balance;
+    }
+    
+    /*_______________________________________________________________*/
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getHash() {
+        return this.hash;
+    }
+    
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+    
+    public Integer getBalance() {
+        return this.balance;
+    }
+    
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+    
+    /*_______________________________________________________________*/
+    
+    @Override
+    public String toString() {
+        return  "u: " + this.name +
+                ", PK:" + this.hash +
+                ", balance: " + this.balance;
+    }
+    
+}
