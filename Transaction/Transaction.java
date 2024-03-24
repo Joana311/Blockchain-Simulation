c11cfef56adb2c7c2d453c98d44b92548f18ee7a
Joana311 <deptop725+3@gmail.com>
2024-03-24 12:19:13 +0100

    Added the initial Transaction class, with the print text

---
+++ Transaction/Transaction.java
@@ -0,0 +1,60 @@
+package Transaction;
+
+import Wallet.*;
+
+public class Transaction {
+
+    private static Integer count = 0;
+
+    private Integer id;
+    private Wallet origin;
+    private Wallet destination;
+    private Integer balance;
+
+    public Transaction(Wallet origin, Wallet destination, Integer balance) {
+        this.origin = origin;
+        this.destination = destination;
+        this.balance = balance;
+
+        count++;
+        this.id = count;
+    }
+
+    /*_______________________________________________________________*/
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Wallet getOrigin() {
+        return this.origin;
+    }
+
+    public void setOrigin(Wallet origin) {
+        this.origin = origin;
+    }
+
+    public Wallet getDestination() {
+        return this.destination;
+    }
+
+    public void setDestination(Wallet destination) {
+        this.destination = destination;
+    }
+
+    public Integer getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    /*_______________________________________________________________*/
+    @Override
+    public String toString() {
+        return "Transaction " + this.id
+                + "| from: " + this.origin.getHash()
+                + ", to:" + this.destination.getHash()
+                + "quantity: " + this.balance;
+    }
+}
