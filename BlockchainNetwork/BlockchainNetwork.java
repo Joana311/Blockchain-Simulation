ec726a8ae32ac88e5c1f14552b8bf102d94743c9
Joana311 <deptop725+3@gmail.com>
2024-03-24 19:58:01 +0100

    Changed the list implementations

--- BlockchainNetwork/BlockchainNetwork.java
+++ BlockchainNetwork/BlockchainNetwork.java
@@ -6,8 +6,9 @@ import Node.*;
 public class BlockchainNetwork {
 
     private String networkName;
-    private HashSet<Node> nodes = new HashSet<>();
-    private HashSet<Subnet> subnets = new HashSet<>();
+    /*private HashSet<Node> nodes = new HashSet<>();
+    private HashSet<Subnet> subnets = new HashSet<>();*/
+    private List<Object> elements = new ArrayList<>();
 
     public BlockchainNetwork(String networkName) {
         this.networkName = networkName;
@@ -22,12 +23,38 @@ public class BlockchainNetwork {
         this.networkName = networkName;
     }
     
-    public HashSet<Node> getNodes() {return this.nodes;}
+    /*public HashSet<Node> getNodes() {return this.nodes;}
     public void setNodes (HashSet<Node> nodes) {this.nodes = nodes;}
     
     public HashSet<Subnet> getSubmits () {return this.subnets;}
-    public void setSubnets (HashSet<Subnet> subnets) {this.subnets = subnets;}
+    public void setSubnets (HashSet<Subnet> subnets) {this.subnets = subnets;}*/
+    
+    public List<Object> getElements () { return this.elements;}
+    public void setElements (List<Object> elements) {this.elements = elements;}
 
     /*_____________________________________________*/
     
+    public BlockchainNetwork connect(Object data)
+    {
+        /* The variable is saved on the correct list */
+        if (this.elements.contains(data))
+            return this;
+        this.elements.add(data);
+        
+        /* The connection is printed */
+        System.out.println(this.networkName + " - new peer connected: " + data);
+        return this;
+    }
+    
+    @Override
+    public String toString() {
+        String buffer;
+        
+        buffer = this.networkName + " consists of " + this.elements.size() + " elements:\n";
+        
+        for (Object current : this.elements)
+            buffer += "* " + current + "\n";
+        
+        return buffer;
+    }
 }
