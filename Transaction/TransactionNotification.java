b0973ef9630352f671cf6d4c160cc31f56bf4b7b
Joana311 <deptop725+3@gmail.com>
2024-04-04 14:34:07 +0200

    Created class to send msg about a transaction

---
+++ Transaction/TransactionNotification.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Transaction;
+
+import Interfaces.*;
+
+public class TransactionNotification implements IMessage {
+
+    private Transaction transaction;
+
+    public TransactionNotification(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Transaction " + this.transaction.getId()
+                + "| from: " + this.transaction.getOriginPublicKey()
+                + ", to: " + this.transaction.getDestinationPublicKey()
+                + ", quantity: " + this.transaction.getBalance();
+    }
+
+}
