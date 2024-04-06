048c1cc3fc2ac40d3ed8e580829df929b9a430ae
Joana311 <deptop725+3@gmail.com>
2024-04-06 18:08:03 +0200

    Added the last test

---
+++ Tests/TesterMainExercise3.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import Transaction.*;
+import Node.*;
+import Wallet.*;
+
+public class TesterMainExercise3 extends TesterMainExercise2 {
+
+    public void createTransactions() {
+        //create a transaction and send it to the network
+        this.miningNode.setMiningMethod(new SimpleMining());
+        this.miningNode.setValidationMethod(new SimpleValidate());
+        this.miningNode2.setMiningMethod(new SimpleMining());
+        this.miningNode2.setValidationMethod(new SimpleValidate());
+        network.broadcast(new TransactionNotification(
+                node.createTransaction(wallet2.getPublicKey(), 100)));
+    }
+
+    public static void main(String[] args) {
+        TesterMainExercise3 tme = new TesterMainExercise3();
+        tme.buildNetwork();
+        tme.createTransactions();
+        System.out.println("End of party!");
+    }
+}
