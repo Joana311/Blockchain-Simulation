2a1f3ce7e419d62a292ad8cb17fbdad085d07a3b
Joana311 <deptop725+3@gmail.com>
2024-04-06 18:10:12 +0200

    Added a transaction getter

--- Transaction/TransactionNotification.java
+++ Transaction/TransactionNotification.java
@@ -14,6 +14,15 @@ public class TransactionNotification implements IMessage {
         this.transaction = transaction;
     }
 
+    /*____________________________________________________________________*/
+    
+    public Transaction getTransaction() {
+        return this.transaction;
+    }
+    
+    /*____________________________________________________________________*/
+    
+    
     @Override
     public String getMessage() {
         return "Transaction " + this.transaction.getId()
