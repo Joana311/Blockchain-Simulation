1ff01cc35e2b37e759ed34afb9f02094cecfc485
Joana311 <deptop725+3@gmail.com>
2024-04-13 20:45:19 +0200

    Added list to non validated transaction and another of the valid ones. Also, there is implemented the recieve of a ValidateBlockRes msg

--- BlockchainComponents/Node.java
+++ BlockchainComponents/Node.java
@@ -5,11 +5,14 @@ import Transaction.*;
 import Wallet.*;
 import Interfaces.*;
 import Exceptions.*;
+import ValidateBlockMsg.*;
 
 public class Node extends BlockchainComponent {
 
     private Wallet wallet;
-    private List<Transaction> transactions = new ArrayList<>();
+    
+    private List<Transaction> nonValidatedTransactions = new ArrayList<>();
+    private List<Transaction> validatedTransactions = new ArrayList<>();
 
     public Node(Wallet wallet) {
         super();
@@ -25,18 +28,55 @@ public class Node extends BlockchainComponent {
         this.wallet = wallet;
     }
 
-    public List<Transaction> getTransactions() {
-        return this.transactions;
+    public List<Transaction> getValidatedTransactions() {
+        return this.validatedTransactions;
+    }
+    
+    public List<Transaction> getNonValidatedTransactions() {
+        return this.nonValidatedTransactions;
     }
 
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
+    
 
     /*____________________________________________________________________*/
     @Override
     public void broadcast(IMessage msg) {
         msg.process(this);
+        
+        if (msg instanceof TransactionNotification transactionNotification) {
+            if (this.nonValidatedTransactions.contains(transactionNotification.getTransaction()) == false)
+                this.nonValidatedTransactions.add(transactionNotification.getTransaction());
+        }
+        
+        if (msg instanceof ValidateBlockRes validateBlockRes)
+        {
+            Transaction transaction = validateBlockRes.getBlock().getTransaction();
+            System.out.println("[" + this.fullName()+ "] Commiting transaction : " +
+                    "Tx-" + transaction.getId() + " in " + this.fullName());
+            System.out.println("[" + this.fullName()+ "] -> Tx details:" + transaction);
+            this.nonValidatedTransactions.remove(transaction);
+            
+            /* If the node is not part of the transaction, stop */
+            if (transaction.getOriginPublicKey() != this.wallet.getPublicKey() &&
+                    transaction.getDestinationPublicKey() != this.wallet.getPublicKey())
+                return ;
+            
+            /* Else, check the transaction (the pdf dont say nothing about an error, so I make a case) */
+            if (validateBlockRes.getState() == false) {
+                System.out.print("[" + this.fullName() + "] No Applied Transaction: " + transaction);
+                return ;
+            }
+            
+            this.validatedTransactions.add(transaction);
+            System.out.print("[" + this.fullName() + "] Applied Transaction: " + transaction);
+            
+            /* Do the changes on the balance */
+            Integer count = (transaction.getOriginPublicKey() == this.wallet.getPublicKey() ? count = -transaction.getBalance() : transaction.getBalance());
+            this.wallet.setBalance(this.wallet.getBalance() + count);
+            
+            System.out.println("[" + this.fullName() + "] New wallet value: " + this.wallet);
+            
+        }
     }
 
     @Override
@@ -53,7 +93,7 @@ public class Node extends BlockchainComponent {
         }
 
         Transaction transaction = new Transaction(originPublicKey, destinationPublicKey, balance);
-        this.transactions.add(transaction);
+        this.nonValidatedTransactions.add(transaction);
         return transaction;
     }
 
